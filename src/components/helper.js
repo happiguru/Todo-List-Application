841d86cffe62762dbefabd80b8b2f0f27c4d7688
happiguru <hguruman@gmail.com>
2021-06-25 15:59:29 +0400

    Add project functions and task UI

--- src/components/helper.js
+++ src/components/helper.js
@@ -23,3 +23,85 @@ const formatDate = (date) => {
     const difference = differenceInDays(new Date(date), new Date());
     return `${difference} day(s) left`;
 }
+
+const openModal = (modal, content) => {
+    const exitModal = ducument.querySelector('.exit-modal');
+    const modalContent = document.querySelector('.modal-content');
+    const modalMessage = document.createElement('p');
+    
+    modal.style.display = 'block';
+    modalMessage.textContent = content;
+    modalContent.append(modalMessage, exitModal);
+
+    exitModal.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+};
+
+const appendDone = (obj, i, taskContainer, doneList, tasksList) => {
+    if (obj.list[i].status) {
+        doneList.appendChild(taskContainer);
+    } else {
+        tasksList.appendChild(taskContainer);
+    }
+};
+
+const newTaskValidation = (modal, title, openModal) => {
+    if (title === '') {
+        openModal(modal, 'Task title must exist!');
+        return false;
+    }
+    return  true;
+};
+
+document.addEventListener('keydown', (e) => {
+    const { keyCode } = e;
+    if (keyCode === 27) {
+        modal.style.display = 'none';
+    }
+});
+
+const addShowDetailsBtn = (container) => {
+    const chevron = document.createElement('span');
+    chevron.innerHTML = '<i class="fas fa-chevron-down"></i>';
+
+    chevron.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (container.style.display === 'block') container.style.display = 'none';
+        else container.style.display = 'block';
+    });
+    return chevron;
+};
+
+const advButtonEvent = (advButton, advOptions) => {
+    advButton.addEventListener('click', () => {
+        if (advOptions.style.display === 'block') {
+            advOptions.style.display = 'none';
+            advButton.innerHTML = 'Advanced Options';
+        } else {
+            advOptions.style.display = 'block';
+            advButton.textContent = 'Hide Options';
+        }
+    });
+};
+
+const addIcon = () => {
+    const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.rel = 'shortcut icon';
+    link.href = logo;
+    document.getElementsByTagName('head')[0].appendChild(link);
+};
+
+export {
+    stylesToPriority,
+    formatDate,
+    openModal,
+    appendDone,
+    newTaskValidation,
+    newTaskValidation,
+    addShowDetailsBtn,
+    modal,
+    addIcon,
+    addLogo,
+    advButtonEvent,
+};
